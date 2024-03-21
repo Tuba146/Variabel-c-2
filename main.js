823a788f2e0fe7a3d1dd191e424a168e96cb0f6a
[Tuba Asif] <[tubaasif146@gmail.com]>
2024-03-21 13:38:17 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//Variables with var
+var firstname = "Tuba";
+console.log(firstname);
+//in terminal there is Tuba
+var firstname;
+console.log(firstname);
+//in terminal there is undefined
+var firstname = "Tuba";
+console.log(firstname);
+//interminal there is Tuba
---
+++ main.ts
@@ -0,0 +1,16 @@
+//Variables with var
+
+
+var firstname : string = "Tuba"
+console.log(firstname)
+//in terminal there is Tuba
+
+var firstname : string
+console.log(firstname)
+//in terminal there is undefined
+
+var firstname = "Tuba"
+console.log(firstname)
+//interminal there is Tuba
+
+
